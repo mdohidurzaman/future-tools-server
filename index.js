9d62d635ed232fb11e87659ba8ef00d7880c9a0f
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-05-22 21:04:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@amazon.38elh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+app.get("/", (req, res) => {
+  res.send("Mongodb is Connected!!");
+});
+
+app.listen(port, () => {
+  console.log("listening to port:", port);
+});
