9dd1bd3e67e48760c26c052af2210a1950f3821b
Md. Ohidurzaman <mozaman.knit@gmail.com>
2022-05-26 20:34:12 +0600

    Added review api

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
       const result = await reviewCollection.insertOne(newReview);
       res.send(result);
     });
+    //Get reviews
+    app.get("/reviews", async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
   } finally {
   }
 }
